57ee9cebe5ec44199510e2044bd27b512619361a
Sebastian Haag <SebastianHaag@macbook-pro-de-sebastien.home>
2015-07-24 02:19:12 +0200

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-$(document).foundation();
+$(document).ready(function() {
+   $(window).load(function() {
+      $("#preload").fadeOut(600);
+   });
+   $('#nav-icon').click(function(){
+      $('#nav-icon, #topBarContent').toggleClass('open');
+      // $('html, body').toggleClass('noScroll');
+      return false;
+   });
+   document.addEventListener('touchmove', function(e) {
+      if($('html.noScroll').length > 0){
+         if (($('nav').has($(e.target)).length > 0)) {
+            e.preventDefault();
+         }
+      }
+   });
+   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      var viewportmeta = document.querySelector('meta[name="viewport"]');
+      if (viewportmeta) {
+         viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+         document.body.addEventListener('gesturestart', function() {
+            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.0';
+         }, false);
+      }
+   }
+   $(document).foundation({
+      reveal: {
+         animation: 'fade',
+      }
+   });
+});
