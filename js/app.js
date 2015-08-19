2eae96d079f7c03002928d92d4c61e4f8f6f7f41
Sebastian Haag <SebastianHaag@MacBook-Pro-de-Sebastien.local>
2015-08-19 23:10:51 +0200

    daily advancement

--- js/app.js
+++ js/app.js
@@ -7,6 +7,33 @@ $(document).ready(function() {
       // $('html, body').toggleClass('noScroll');
       return false;
    });
+   $('.dataclose').click(function(){
+      $('#nav-icon, #topBarContent').removeClass('open');
+      return false;
+   });
+
+   var $root = $('html, body');
+      $('a').click(function() {
+      var href = $.attr(this, 'href');
+         $root.animate({
+            scrollTop: $(href).offset().top
+         }, 1000, function () {
+            window.location.hash = href;
+      });
+      return false;
+   });
+
+   var swiper = new Swiper('.swiper-container', {
+      speed: 600,
+      pagination: '.swiper-pagination',
+      paginationClickable: '.swiper-pagination',
+      effect: 'fade',
+      autoplay: 6000,
+      loop: true,
+      preloadImages: false,
+      lazyLoading: true
+   });
+
    document.addEventListener('touchmove', function(e) {
       if($('html.noScroll').length > 0){
          if (($('nav').has($(e.target)).length > 0)) {
@@ -14,15 +41,15 @@ $(document).ready(function() {
          }
       }
    });
-   // if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-   //    var viewportmeta = document.querySelector('meta[name="viewport"]');
-   //    if (viewportmeta) {
-   //       viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
-   //       document.body.addEventListener('gesturestart', function() {
-   //          viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.0';
-   //       }, false);
-   //    }
-   // }
+   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      var viewportmeta = document.querySelector('meta[name="viewport"]');
+      if (viewportmeta) {
+         viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+         document.body.addEventListener('gesturestart', function() {
+            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.0';
+         }, false);
+      }
+   }
    $(document).foundation({
    });
 });
