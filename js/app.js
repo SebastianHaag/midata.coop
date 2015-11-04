8aba7894071bb570494a865159001e2b588021da
Sebastian Haag <SebastianHaag@MacBook-Pro-de-Sebastien.local>
2015-11-04 18:55:21 +0100

    scheme & spaces fix

--- js/app.js
+++ js/app.js
@@ -11,18 +11,6 @@ $(document).ready(function() {
       $('#nav-icon, #topBarContent').removeClass('open');
       return false;
    });
-
-   var $root = $('html, body');
-      $('a').click(function() {
-      var href = $.attr(this, 'href');
-         $root.animate({
-            scrollTop: $(href).offset().top
-         }, 1000, function () {
-            window.location.hash = href;
-      });
-      return false;
-   });
-
    document.addEventListener('touchmove', function(e) {
       if($('html.noScroll').length > 0){
          if (($('nav').has($(e.target)).length > 0)) {
@@ -41,4 +29,54 @@ $(document).ready(function() {
    }
    $(document).foundation({
    });
+
+   $(window).scroll(function () {
+        if ($(document).scrollTop() > 100) {
+            $("nav").addClass("scrolledlogo");
+        } else {
+            $("nav").removeClass("scrolledlogo");
+        }
+   });
 });
+
+//IN PAGE SCROLL - ACTIVE LINKS
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 50
+         },
+         1000, function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('nav ul li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top - 70 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
