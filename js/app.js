18225ec5884b83f441d844078cf76284efaa6b7d
Sebastian Haag <SebastianHaag@MacBook-Pro-de-Sebastien.local>
2015-11-06 17:51:14 +0100

    ppl aso

--- js/app.js
+++ js/app.js
@@ -44,11 +44,11 @@ $(document).ready(function () {
     $(document).on("scroll", onScroll);
 
     //smoothscroll
-    $('a[href^="#"]').on('click', function (e) {
+    $('nav ul li a').on('click', function (e) {
         e.preventDefault();
         $(document).off("scroll");
 
-        $('a').each(function () {
+        $('nav ul li a').each(function () {
             $(this).removeClass('active');
         })
         $(this).addClass('active');
